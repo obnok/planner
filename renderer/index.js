599b9f0364c58895a498de9ef6447eb52bfce0f1
David Simon <110782993+obnok@users.noreply.github.com>
2023-02-03 17:49:38 +0000

    Uniform navbar using JS

---
+++ renderer/index.js
@@ -0,0 +1,28 @@
+function activeNav() {
+    var navItem = document.getElementById('uniNav').children; 
+    console.log(document.title)
+    for(let i = 0; i < 6; i++) {
+        console.log(navItem[i].firstChild.innerHTML)
+        if(document.title == navItem[i].firstChild.innerHTML) {
+            navItem[i].style.backgroundColor = '#8b579f';
+        }
+    }
+}
+
+function loadNav() {
+    const template = document.createElement('template');
+    template.innerHTML = `
+    <nav>
+        <ul id="uniNav" class="uni default">
+            <li><a href="index.html">Menu</a></li>
+            <li><a href="calendar.html">Calendar</a></li>
+            <li><a href="tasks.html">Tasks</a></li>
+            <li><a href="notes.html">Notes</a></li>
+            <li><a href="timers.html">Timers</a></li>
+            <li><a href="settings.html">Settings</a></li>
+        </ul>
+    </nav>
+    `;
+    document.getElementById('loadNav').appendChild(template.content);
+    activeNav();
+}
