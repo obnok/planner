7dc8df1a4a38ab3ac5dad57c17393374b96ff171
David Simon <110782993+obnok@users.noreply.github.com>
2023-02-09 05:35:33 +0000

    Universal nav bar using innerHTML

--- renderer/index.js
+++ renderer/index.js
@@ -1,40 +1,35 @@
-function activeNav() {
-    var navItem = document.getElementById('uniNav').children; 
-    console.log(document.title)
-    for(let i = 0; i < 6; i++) {
-        console.log(navItem[i].firstChild.innerHTML)
-        if(document.title == navItem[i].firstChild.innerHTML) {
-            navItem[i].style.backgroundColor = '#8b579f';
-        }
-    }
-}
+// document.addEventListener('DOMContentLoaded', function() {
+//     var calendarEl = document.getElementById('calendar');
+//     var calendar = new FullCalendar.Calendar(calendarEl, {
+//     headerToolbar: {
+//         left: 'today prev,next',
+//         center: 'title',
+//       initialView: 'dayGridMonth',
+//       selectable: true
+//     },
+//     });
+//     calendar.render();
+//   });
 
-function loadNav() {
-    const template = document.createElement('template');
-    template.innerHTML = `
-    <nav>
-        <ul id="uniNav" class="uni default">
+document.addEventListener('DOMContentLoaded', function() {
+    console.log('Please');
+    var edit = document.getElementById('nav-bar');
+    edit.innerHTML = `
+        <nav>
+            <ul id="uni" class="uni default">
             <li><a href="index.html">Menu</a></li>
             <li><a href="calendar.html">Calendar</a></li>
             <li><a href="tasks.html">Tasks</a></li>
             <li><a href="notes.html">Notes</a></li>
             <li><a href="timers.html">Timers</a></li>
             <li><a href="settings.html">Settings</a></li>
-        </ul>
-    </nav>
-    `;
-    document.getElementById('loadNav').appendChild(template.content);
-    activeNav();
-}
-
-document.addEventListener('DOMContentLoaded', function() {
-    var calendarEl = document.getElementById('calendar');
-    var calendar = new FullCalendar.Calendar(calendarEl, {
-    headerToolbar: {
-        left: 'today prev,next',
-        center: 'title',
-      initialView: 'dayGridMonth',
-      selectable: true,
-    });
-    calendar.render();
-  });
+            </ul>
+        </nav>
+        `;
+    var navItem = document.getElementById('uni').children; 
+    for(let i = 0; i <= 5; i++) {
+        if(document.title == navItem[i].firstChild.innerHTML) {
+            navItem[i].style.backgroundColor = '#8b579f';
+        }
+    }
+});
